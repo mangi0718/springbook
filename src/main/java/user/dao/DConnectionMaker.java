eedba19e557d60bb5f5d539790b8e6cb6200f211
kimmangi <kmg0718@gmail.com>
2023-03-07 01:32:51 +0900

    spring book study

---
+++ src/main/java/user/dao/DConnectionMaker.java
@@ -0,0 +1,18 @@
+package user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        // D사의 독자적인 방법으로 Connection을 생성하는 코드
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook",
+                "spring", "book");
+        return c;
+    }
+}
